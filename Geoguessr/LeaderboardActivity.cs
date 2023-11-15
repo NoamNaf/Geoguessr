d9489d6ec6957f7d309a1ab71fb3afc9d65e926c
noamnaf <noamnaf@gmail.com>
2023-11-15 20:42:03 +0200

    Leaderboard style change

--- Geoguessr/LeaderboardActivity.cs
+++ Geoguessr/LeaderboardActivity.cs
@@ -12,15 +12,25 @@ using System.Text;
 namespace Geoguessr
 {
     [Activity(Label = "LeaderboardActivity")]
-    public class LeaderboardActivity : Activity
+    public class LeaderboardActivity : Activity, View.IOnClickListener
     {
+        private Button returnbtn;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.leaderboard);
+            returnbtn = FindViewById<Button>(Resource.Id.returnbtn);
+            returnbtn.SetBackgroundResource(Resource.Drawable.rounded_corner);
+
+            returnbtn.SetOnClickListener(this);
 
             // Create your application here
+        }
+        public void OnClick(Android.Views.View view)
+        {
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
         }
     }
 }
