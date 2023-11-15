76c266ab209c5b10c3769eb6ecddb8fa99e7d926
noamnaf <noamnaf@gmail.com>
2023-11-15 15:15:45 +0200

    adding login and register screens

---
+++ Geoguessr/LoginActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geoguessr
+{
+    [Activity(Label = "LoginActivity")]
+    public class LoginActivity : Activity, View.IOnClickListener
+    {
+        private EditText username;
+        private EditText password;
+        private Button loginbtn;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.login);
+            loginbtn = FindViewById<Button>(Resource.Id.buttonLogin);
+            username = FindViewById<EditText>(Resource.Id.username);
+            password = FindViewById<EditText>(Resource.Id.editPassword);
+            loginbtn.SetBackgroundResource(Resource.Drawable.rounded_corner);
+            loginbtn.SetOnClickListener(this);
+            // Create your application here
+        }
+        public void OnClick(View v)
+        {
+            if(loginbtn != null && password != null)
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            }
+            else
+            {
+                Toast.MakeText(this, "Please enter a legal username and password", ToastLength.Long).Show();
+            }
+        }
+    }
+}
---
+++ Geoguessr/RegisterActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geoguessr
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity, View.IOnClickListener
+    {
+        private EditText newUsername;
+        private EditText newPassword;
+        private Button registerbtn;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.register);
+            newUsername = FindViewById<EditText>(Resource.Id.username);
+            newPassword = FindViewById<EditText>(Resource.Id.newPassword);
+            registerbtn = FindViewById<Button>(Resource.Id.buttonRegister);
+            registerbtn.SetBackgroundResource(Resource.Drawable.rounded_corner);
+            registerbtn.SetOnClickListener(this);
+            // Create your application here
+        }
+        public void OnClick(View v)
+        {
+            if(newUsername != null && newPassword != null)
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            }
+            else
+            {
+                Toast.MakeText(this, "Please enter a legal username and password", ToastLength.Long).Show();
+            }
+        }
+    }
+}
