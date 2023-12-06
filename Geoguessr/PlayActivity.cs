b5338292487aaf93a346169bdf69454683e432c1
noamnaf <noamnaf@gmail.com>
2023-12-06 15:22:49 +0200

    working google maps

--- Geoguessr/PlayActivity.cs
+++ Geoguessr/PlayActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Gms.Maps;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -15,14 +16,15 @@ using System.Text;
 namespace Geoguessr
 {
     [Activity(Label = "Play")]
-    public class PlayActivity : Activity
+    public class PlayActivity : Activity, IOnMapReadyCallback
     {
-        private string round;
+        private string round;   
         private TextView roundview;
         private Button guessbtn;
         private Button hintbtn;
         private Button openmapbtn;
         private GameLogic gameLogic;
+        private GoogleMap googleMap;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -46,11 +48,28 @@ namespace Geoguessr
             gameLogic.NextRound();
             this.round = "Round " + gameLogic.GetRoundNum() + "/5";
 
-            roundview.Text = "Round 1/5";
+            roundview.Text = round;
 
             guessbtn.Click += guessbtn_Click;
             hintbtn.Click += Hintbtn_Click;
             openmapbtn.Click += OpenMapbtn_Click;
+
+            var mapFrag = MapFragment.NewInstance();
+            FragmentManager.BeginTransaction()
+                                    .Add(Resource.Id.map, mapFrag, "map_fragment")
+                                    .Commit();
+            //var mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFrag.GetMapAsync(this);
+        }
+        public void OnMapReady(GoogleMap map)
+        {
+            // Do something with the map, i.e. add markers, move to a specific location, etc.
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
         private void Hintbtn_Click(object sender, EventArgs e)
